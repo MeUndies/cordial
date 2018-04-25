a7bd39ba90f02a2116c8eaae2190b4842294b72f
Andrew Hood <andrewhood125@gmail.com>
2018-04-25 14:08:56 -0700

    Add Cordial::Contact.new to create contacts

--- lib/cordial/contacts.rb
+++ lib/cordial/contacts.rb
@@ -7,5 +7,15 @@ module Cordial
     def self.find(email:)
       client.get("/contacts/#{email}")
     end
+
+    def self.create(email:, attribute_list:)
+      client.post('/contacts', body: {
+        channels: {
+          email: {
+            address: email
+          }
+        },
+      }.merge(attribute_list))
+    end
   end
 end
