ccecfd0802c41c4a6c501e1326c1e349e40dd1a5
Andrew Hood <andrewhood125@gmail.com>
2018-04-25 14:08:56 -0700

    Add Cordial::Contact.new to create contacts

--- spec/cordial/contacts_spec.rb
+++ spec/cordial/contacts_spec.rb
@@ -1,15 +1,30 @@
 require "spec_helper"
 
 RSpec.describe Cordial::Contacts do
+  let(:email) { 'cordial@example.com' }
+
   describe '#find' do
-
     subject { described_class.find(email: email) }
-
-    let(:email) { 'cordial@example.com' }
 
     it 'has a correctly formatted request url' do
       expect(subject.request.last_uri.to_s).to eq 'https://api.cordial.io/v1/contacts/cordial@example.com'
     end
+  end
+
+  describe '#create' do
+     subject { described_class.create(email: email, attribute_list: attribute_list) }
+
+     let(:attribute_list) do
+       { first_name: 'Cordial' }
+     end
+
+     it 'has a correctly formatted request url' do
+      expect(subject.request.last_uri.to_s).to eq 'https://api.cordial.io/v1/contacts'
+     end
+
+     it 'has the correct payload' do
+       expect(subject.request.raw_body).to eq 'channels[email][address]=cordial%40example.com&first_name=Cordial'
+     end
   end
 end
 
