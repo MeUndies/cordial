33d69a37b25b6549457c62e3738836cc7d1da676
Andrew Hood <andrewhood125@gmail.com>
2018-11-03 16:55:30 -0700

    Update Yardoc for Cordial::Orders

--- lib/cordial/orders.rb
+++ lib/cordial/orders.rb
@@ -1,5 +1,6 @@
 module Cordial
-  # Wraps all interaction with the Order resource.
+  # Pragmatic wrapper around the orders REST Api.
+  #
   # @see https://api.cordial.io/docs/v1/#!/orders
   class Orders
     include ::HTTParty
@@ -8,52 +9,51 @@ module Cordial
     # Find an order
     # @example Usage
     #  Cordial::Orders.find(id: 1)
-    # @example Response when the order was found
-    # {
-    #   "_id"=>"5b3a774a2cab4e1a59d0cc6d",
-    #   "orderID"=>"1",
-    #   "purchaseDate"=>"2015-01-10T01:47:43+0000",
-    #   "items"=>[
-    #     {
-    #       "productID"=>"1",
-    #       "sku"=>"123",
-    #       "name"=>"Test product",
-    #       "attr"=>{
-    #         "color"=>"blue",
-    #         "size"=>"L"
-    #        },
-    #       "amount"=>0
-    #     }
-    #   ],
-    #   "cID"=>"5aea409bbb3dc2f9bc27158f",
-    #   "totalAmount"=>0
-    # }
     #
-    # @example Response when the order was not found.
-    #  {"error"=>true, "message"=>"record not found"}
+    # @param id [String|Fixnum] The order's id.
+    #
+    # @return [Hash]
+    # @return [{"error"=>true, "message"=>"record not found"}]
     def self.find(id:)
       client.get("/orders/#{id}")
     end
 
     # Create a new order.
     #
-    # This endpoint does not support the idea of an upsert like others do.
-    # Subsequent calls will fail.
+    # @note This endpoint does not support upsert.
     #
     # @example Usage.
-    # Cordial::Orders.create({...})
+    #   Cordial::Orders.create({...})
     #
-    # @example response when the orderID is not on cordial
-    # {"success"=>true}
-    #
-    # @example Response when orderID already exist on cordial.
-    # {"error"=>true, "messages"=>"ID must be unique"}
+    # @return [{"success"=>true}]
+    # @return [{"error"=>true, "messages"=>"ID must be unique"}]
     def self.create(options)
       order = Cordial::Order.new(options)
       client.post('/orders', body: order.to_json)
     end
 
-    # List orders in Cordial
+    # List Orders matching criteria.
+    #
+    # @example
+    #   Cordial::Orders.index(
+    #     fields: 'orderID,purchaseDate',
+    #     purchaseDate: { 'lt': '2018-10-21 11:11:11' },
+    #     per_page: 5
+    #   )
+    #
+    # @option options [String] :fields Comma delimited string of fields to be returned.
+    # @option options [String] :cID Where Customer ID is..
+    # @option options [String] :email Where email is..
+    # @option options [Hash] :purchaseDate When purchased on, before, and after (YYYY-MM-DD HH:ii:ss)
+    #   {
+    #     'eq': '2018-10-31 11:59:59',
+    #     'lt': '2018-10-31 11:59:59',
+    #     'gt': '2018-10-31 11:59:59'
+    #     }
+    # @option options [Fixnum] :page Which page of results
+    # @option options [Fixnum] :per_page How many results to return on a page (max: 10,000)
+    #
+    # @return [Array<Hash>]
     def self.index(options = {})
       client.get(
         '/orders',
